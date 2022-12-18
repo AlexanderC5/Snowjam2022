e1e2e08905869fa7c0dea22f25678bffd5dbaab5
AlexanderC5 <91765309+AlexanderC5@users.noreply.github.com>
2022-12-17 22:25:31 -0800

    Settings page complete

--- Snowjam2022 Team 2/Assets/Scripts/Settings.cs
+++ Snowjam2022 Team 2/Assets/Scripts/Settings.cs
@@ -6,9 +6,10 @@ public class Settings : MonoBehaviour
 {
     public static Settings Instance;
 
+    public float masterVolume = 1f;
     public float sfxVolume = 1f;
     public float musVolume = 1f;
-    public float animationSpeed = 1f;
+    public float animationSpeed = 1.5f;
     public int difficulty = 1;
 
     public float[] zones = { 300f, 600f, 900f };
@@ -25,5 +26,26 @@ public class Settings : MonoBehaviour
 
         Instance = this;
         DontDestroyOnLoad(gameObject);
+    }
+
+    public void SetAnimationSpeed(float spd)
+    {
+        animationSpeed = spd;
+    }
+    public void SetMasterVolume(float vol)
+    {
+        masterVolume = vol;
+    }
+    public void SetMusVolume(float vol)
+    {
+        musVolume = vol;
+    }
+    public void SetSfxVolume(float vol)
+    {
+        sfxVolume = vol;
+    }
+    public void SetEnemyDifficulty(float dif)
+    {
+        difficulty = (int) dif;
     }
 }
